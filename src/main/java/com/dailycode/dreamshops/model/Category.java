57c0655dab8a43bd816dd83fc7c0c36e29b7641a
Aviral Mehra <aviral.mehra008@gmail.com>
2024-09-07 20:13:43 +0530

    Added Model classes

---
+++ src/main/java/com/dailycode/dreamshops/model/Category.java
@@ -0,0 +1,16 @@
+package com.dailycode.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@Data
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
